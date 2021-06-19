397454012298c9b6c06d70fb9376bb1afe8e51e2
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-06-19 12:24:54 +0500

    reconstruction 19.06.2021

---
+++ 2020-2021/Paragraf 59/A. Liniya.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void printString(int n) {
+	for (int i = 1; i <= n; i++) {
+		cout << "-";
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	printString(n);
+
+	return 0;
+}
---
+++ 2020-2021/Paragraf 59/C. Cifri chisla -1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void reverse(int n) {
+	while (n != 0) {
+		cout << n % 10 << endl;
+		n /= 10;
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	reverse(n);
+	return 0;
+}
